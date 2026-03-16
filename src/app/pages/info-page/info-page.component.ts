7ea3106103acc487d4cfa00253e9c9c05de2f6e0
I753825 <v.lazic@sap.com>
2026-03-16 18:24:11 +0100

    Fix build error: Add missing InfoPageComponent files

---
+++ src/app/pages/info-page/info-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-info-page',
+  templateUrl: './info-page.component.html',
+  styleUrls: ['./info-page.component.css']
+})
+export class InfoPageComponent implements OnInit {
+  type: string = '';
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.type = params['type'];
+      window.scrollTo(0, 0);
+    });
+  }
+}
